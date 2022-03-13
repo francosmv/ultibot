ebbc6be9bf4bfd416ddc39e2c1c5c183cc7bae51
Franco Santa Maria <francosmv@gmail.com>
2022-03-13 12:33:58 -0600

    fix async stuff not working in game session, minor cleanup

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ from dotenv import load_dotenv
 import random
 import math
 
+global ready_session
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+ready_session = None
 
 class gameStateException(Exception):
     pass
 
 class game_session:
-    def __init__(self, message):
+    def __init__(self):
+        self.guild = None
+        self.cmd_channel = None
+        self.neutral_vc = None
+        self.team_blu = []
+        self.team_red = []
+        self.teams_msg = None
+    async def create_session(self, message):
+        global ready_session
         self.guild = msg_guild = message.guild
         self.cmd_channel = message.channel
         self.neutral_vc = None
@@ -25,38 +37,35 @@ class game_session:
                 break
         if((self.neutral_vc != None) and (len(self.neutral_vc.members) > 1)):
             player_list = self.neutral_vc.members
-            self.team_blu = []
-            self.team_red = []
             #With odd number of players, this makes blu team always 1 bigger than red
             #TODO: On odd number of players, leave out one member, rotate who gets left out
             team_blu_sz = math.ceil(len(player_list)/2)
             for i in range(team_blu_sz):
-                print(len(player_list))
                 idx = random.randint(0, len(player_list)-1)
                 self.team_blu.append(player_list[idx])
                 del player_list[idx]
             self.team_red = player_list
+            self.teams_msg = await self.cmd_channel.send(embed=self.print_teams())
+            check_emoji="\U00002705"
+            await self.teams_msg.add_reaction(check_emoji)
+            ready_session = self
         elif(self.neutral_vc != None): # Have to have two members in voice channel, makes it so I don't have to deal with empty lists
-            raise gameStateException("You must have at least two members in your voice channel to generate teams")
+            await self.cmd_channel.send("You must have at least two members in your voice channel to generate teams")
+            raise gameStateException()
         else: # User not in a channel on that server
-            raise gameStateException("You must be in a voice channel to generate teams")
-
+            await self.cmd_channel.send("You must be in a voice channel to generate teams")
+            raise gameStateException()
     def print_teams(self):
         # Create and send the team table to the channel
-            embed = discord.Embed(title=f"__**Teams:**__", color=0x00109c)
-            #TODO: gross, make this a function
-            team_blu_list_str = ""
-            team_red_list_str = ""
-            for player in self.team_blu:
-                team_blu_list_str = team_blu_list_str + player.name + "\n"
-            for player in self.team_red:
-                team_red_list_str = team_red_list_str + player.name + "\n"
-            embed.add_field(name=f"**BLU**", value=team_blu_list_str, inline=True)
-            embed.add_field(name=f"**RED**", value=team_red_list_str, inline=True)
+            embed = discord.Embed(title=f"**Teams:**", color=0x00109c)
+            embed.add_field(name=f"**BLU**", value=self.create_team_list_str(self.team_blu), inline=True)
+            embed.add_field(name=f"**RED**", value=self.create_team_list_str(self.team_red), inline=True)
             return embed
-    def register_session_ready(self, message):
-        self.teams_msg = message
-        # Set the global ready session to self
+    def create_team_list_str(self, team):
+        team_list_str = ""
+        for player in team:
+            team_list_str = team_list_str + player.name + "\n"
+        return team_list_str
 
 
 client = discord.Client()
@@ -76,17 +85,14 @@ async def on_message(message):
         return
 
     if(bot_cmd == "game"):
-        #TODO: Set game_session up to work correctly with async stuff so that these steps can just be put in the constructor
+        #TODO: Add "exc" argument to tell bot to explicitly exclude a player
+        print(ready_session)
         try:
-            session = game_session(message)
+            session = game_session()
+            await session.create_session(message)
         except gameStateException as e:
-            await message.channel.send(e)
             return
-        teams_msg = await session.cmd_channel.send(embed=session.print_teams())
-        check_emoji="\U00002705"
-        await teams_msg.add_reaction(check_emoji)
-        session.register_session_ready(teams_msg)
-
+        print(ready_session)
     # elif(bot_cmd == "rollmap")
     # elif(bot_cmd == "addmap")
     # elif(bot_cmd == "delmap")
